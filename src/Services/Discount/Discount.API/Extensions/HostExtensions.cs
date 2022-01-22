2d4c28d68c35f9a495c67c558d5dff22331736f8
Vaghinak Vardanyan <vaghinak.vardanyan@technamin.com>
2022-01-22 19:18:56 +0400

    Added Discount API database migration

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Npgsql;
+
+namespace Discount.API.Extensions;
+
+public static class HostExtensions
+{
+    public static IHost MigrateDatabase<TContext>(this IHost host, int retry = 0)
+    {
+        using var scope = host.Services.CreateScope();
+
+        var services = scope.ServiceProvider;
+        var configuration = services.GetRequiredService<IConfiguration>();
+        var logger = services.GetRequiredService<ILogger<TContext>>();
+
+        try
+        {
+            logger.LogInformation("Migrating postgresql database.");
+
+            using var connection = new NpgsqlConnection
+            (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            connection.Open();
+
+            using var command = connection.CreateCommand();
+
+            command.CommandText = "drop table if exists coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"create table Coupon(Id serial primary key,
+                                                        ProductName varchar(24) not null,
+                                                        Description text,
+                                                        Amount int)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"insert into Coupon(ProductName, Description, Amount)
+                                    values
+                                        ('Iphone X', 'Iphone Discount', 150),
+                                        ('Samsung 10', 'Samsung Discount', 100)";
+            command.ExecuteNonQuery();
+
+            logger.LogInformation("Migrated postgresql database.");
+        }
+        catch (NpgsqlException ex)
+        {
+            logger.LogError(ex, "An error occured while migrating the postgresql database");
+
+            if (retry < 50)
+            {
+                Thread.Sleep(2000);
+                MigrateDatabase<TContext>(host, ++retry);
+            }
+        }
+
+        return host;
+    }
+}
+
