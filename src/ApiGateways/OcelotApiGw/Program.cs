1784cf7748ad3473f6de8d9489b39f418362ff4b
Vaghinak Vardanyan <vaghinak.vardanyan@technamin.com>
2022-02-07 21:26:26 +0400

    configuring OcelotApiGw

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+// Add services to the container.
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseOcelot().Wait();
+
+app.Run();
